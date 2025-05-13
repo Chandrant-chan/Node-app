898ec9d88e5f5789a07c5a5c9ed76cc2607fcea3
Chandran Tamilselvan <126679104+Chandrant-chan@users.noreply.github.com>
2025-05-13 13:53:53 +0530

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3001;
 
 app.get('/', (req, res) => {
-  res.send('Hello from Node.js and Express!');
+  res.send('<h1 style="color: blue">Hello from Node.js and Express!</h1>');
 });
 //check
 app.listen(port, () => {
